f9b2fa3b4adf280ebaf5242dbf15227f13de8b55
brendon cecilio <brendon.cecilio@gmail.com>
2019-12-06 17:13:23 -0500

    added initial UI elements

---
+++ XKCDComicViewer/XKCDComicViewer/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  XKCDComicViewer
+//
+//  Created by Brendon Cecilio on 12/6/19.
+//  Copyright © 2019 Brendon Cecilio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var comicStepper: UIStepper!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func mostRecentButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func randomButtonPressed(_ sender: UIButton) {
+    }
+}
+
