a3d9db5911b6e086ea449257d4c3824499c60eeb
brendon cecilio <brendon.cecilio@gmail.com>
2019-12-10 23:32:09 -0500

    added initial syntax for VC

--- PokemonCards/PokemonCards/ViewController.swift
+++ PokemonCards/PokemonCards/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var pokemonInfo = [PokemonCards]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-
-
+    
+    func loadData(urlString: String) {
+        let pokemon = PokemonCardAPI.getCards(with: urlString) { (result) in
+            switch result {
+            case .failure(let appError):
+                print("\(appError)")
+            case .success(let pokemonData):
+                DispatchQueue.main.async{
+                    
+                }
+            }
+        }
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokemonInfo.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cardCell", for: indexPath)
+        
+        let pokemonCell = pokemonInfo[indexPath.row]
+        cell.textLabel?.text = pokemonCell.
+        return cell
+    }
+    
+    
+}
